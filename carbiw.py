8e271d38f090dfe37507d1d4fc2eb4f80abdebe3
26864-carbiw <26864@bbw.ac.th>
2026-06-11 14:22:55 +0700

    โปรเเกรมคำนวณค่าใช้จ่าย เวอร์ชั่น 1
    11/มิ.ย/69
    เข้าใจครับสนุกมากมากชอบมากมาก

---
+++ carbiw.py
@@ -0,0 +1,16 @@
+name = input("คุณชื่ออะไร? ")
+age = int(input("คุณอายุเท่าไหร่? "))
+print("สวัสดีครับ", name)
+print("อีก 1 ปี คุณจะอายุ", age + 1, "ปี")
+a = 10
+b = 2.5
+c = input("ใส่ข้อความ: ")
+print(type(a))
+print(type(b))
+print(type(c))
+a = int(input("ใส่จำนวนเต็ม: "))
+b = float(input("ใส่ทศนิยม: "))
+c = a + b
+d = a - int(b)
+e = b * 2
+print(c, d, e)
