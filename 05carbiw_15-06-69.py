75f3a2eccf626325b12e964d04b8c30f05491085
26864-carbiw <26864@bbw.ac.th>
2026-06-15 09:40:03 +0700

    ฝึกเขียนโปรเเกรมเงื่อนไขโดยใช้คำสั่ง if else

---
+++ 05carbiw_15-06-69.py
@@ -0,0 +1,9 @@
+print("โปรเเกรมเลขคู๋หรือเเลขคี่กันนะ")
+number = int(input("Enter your number: "))
+
+if number %2 == 0:
+    print("เลขคู๋")
+else:
+    print("เลขคี่")
+
+print("โดย นายบรรณวิชญ์ วิเศษสุข เลขที่5 ม.4/4 \n ขอบคุณครับ")
