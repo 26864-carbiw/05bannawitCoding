fdae35c72c580a080716adaed8cfa5293a4c97ca
26864-carbiw <26864@bbw.ac.th>
2026-07-06 09:12:34 +0700

    Add files via upload
    สูตรคูณเวอร์ชั่น 1

---
+++ carbiw Multiplication table.py
@@ -0,0 +1,8 @@
+print("\nสูตรคูณเวอร์ชั่น 1")
+number = int(input("\nกรุณาใส่แม่สูตรคูณ1จำนวน : "))
+print("\nสูตรคูณขอคุณคือ")
+for i in range(1,13):
+    print(number, "x",i,"=",number*i)
+print("\nจัดทำโดย นายบรรณวิชญ์ วิเศษสุข")
+
+
